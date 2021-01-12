8a150cdda9474ffe891337fd3de7db5e14acceaf
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2021-01-12 20:32:29 +0100

    Agregar archivos de proyecto.

---
+++ Peliculas/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Peliculas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog ofd;
+        ObservableCollection<Pelicula> lista;
+        enum Generos
+        {
+            Comedia, Drama, Acción, Terror, CienciaFicción
+        };
+
+        enum Dificultad
+        {
+            Fácil, Normal, Difícil
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Directory.GetCurrentDirectory();
+            lista = new ObservableCollection<Pelicula>();
+            getSamples();
+            //lista.Add(new Pelicula("Fast Furious 9", "A todo gas 9", "Media", "Acción", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fast-and-furious-9-poster-1580411801.jpeg"));
+            //lista.Add(new Pelicula("Animales Fantásticos", "Los crímenes de Grindelwald", "Media", "Acción", "https://pics.filmaffinity.com/Animales_fant_sticos_Los_cr_menes_de_Grindelwald-693286072-large.jpg"));
+            peliculasListBox.DataContext = lista;
+            generoComboBox.ItemsSource = Enum.GetValues(typeof(Generos));
+            dificultadComboBox.ItemsSource = Enum.GetValues(typeof(Dificultad));
+        }
+
+        private void getSamples()
+        {
+            using (StreamReader jsonStream = File.OpenText("datos.json"))
+            {
+                var json = jsonStream.ReadToEnd();
+                lista = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(json);
+            }
+            peliculasListBox.DataContext = lista;
+
+            //
+            //
+            //      ARRREGLA ESTA CHAPUZA
+            //
+            //
+
+            peliculasListBox.SelectedItem = lista[0];
+        }
+
+        private void cargarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            ofd.Filter = "Archivos JSON (*.json)|*.json";
+            ofd.ShowDialog();
+            try
+            {
+                ofd.OpenFile();
+
+                using (StreamReader jsonStream = File.OpenText(ofd.FileName))
+                {
+                    var json = jsonStream.ReadToEnd();
+                    lista = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(json);
+                }
+                MessageBox.Show("Éxito al cargar", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                peliculasListBox.DataContext = lista;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe seleccionar un archivo", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void guardarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Archivos JSON (*.json)|*.json";
+            sfd.ShowDialog();
+            string nombreFichero = sfd.FileName;
+            if (string.IsNullOrEmpty(nombreFichero))
+            {
+                MessageBox.Show("No se guardó la información", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                string datos = JsonConvert.SerializeObject(lista);
+                File.WriteAllText(nombreFichero, datos);
+                MessageBox.Show("Guardado correctamente", "Guardado", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void examinarButton_Click(object sender, RoutedEventArgs e)
+        {
+            ofd.Filter = "Archivos de imágenes (*.bmp, *.jpg, *.jpeg)|*.bmp;*.jpg;*.jpeg";
+            ofd.Title = "Examinar";
+            ofd.ShowDialog();
+            if (!string.IsNullOrEmpty(ofd.FileName))
+                imagenTextBox.Text = ofd.FileName;
+        }
+
+        private void addPeliculaButton_Click(object sender, RoutedEventArgs e)
+        {
+            string titulo = tituloTextBox.Text;
+            string pista = pistaTextBox.Text;
+            string dificultad = dificultadComboBox.SelectedItem.ToString();
+            string imagen = imagenTextBox.Text;
+            string genero = generoComboBox.SelectedItem.ToString();
+            lista.Add(new Pelicula(titulo, pista, dificultad, genero, imagen));
+        }
+
+        private void eliminarPeliculaButton_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Remove((Pelicula)peliculasListBox.SelectedItem);
+        }
+
+        private void deseleccionarPeliculaButton_Click(object sender, RoutedEventArgs e)
+        {
+            peliculasListBox.UnselectAll();
+        }
+    }
+}
---
+++ Peliculas/Pelicula.cs
@@ -0,0 +1,94 @@
+﻿using System.ComponentModel;
+
+
+namespace Peliculas
+{
+    public class Pelicula : INotifyPropertyChanged
+    {
+
+        private string _titulo;
+        public string Titulo
+        {
+            get => _titulo;
+            set
+            {
+                if (_titulo != value)
+                {
+                    _titulo = value;
+                    NotifyPropertyChanged("Titulo");
+                }
+            }
+        }
+
+        private string _pista;
+        public string Pista
+        {
+            get => _pista;
+            set
+            {
+                if (_pista != value)
+                {
+                    _pista = value;
+                    NotifyPropertyChanged("Pista");
+                }
+            }
+        }
+
+        private string _dificultad;
+        public string Dificultad
+        {
+            get => _dificultad;
+            set
+            {
+                if (_dificultad != value)
+                {
+                    _dificultad = value;
+                    NotifyPropertyChanged("Dificultad");
+                }
+            }
+        }
+
+        private string _genero;
+        public string Genero
+        {
+            get => _genero;
+            set
+            {
+                if (_genero != value)
+                {
+                    _genero = value;
+                    NotifyPropertyChanged("Genero");
+                }
+            }
+        }
+
+        private string _imagen;
+        public string Imagen
+        {
+            get => _imagen;
+            set
+            {
+                if (_imagen != value)
+                {
+                    _imagen = value;
+                    NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+
+        public Pelicula(string titulo, string pista, string dificultad, string genero, string imagen)
+        {
+            Titulo = titulo;
+            Pista = pista;
+            Dificultad = dificultad;
+            Genero = genero;
+            Imagen = imagen;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
